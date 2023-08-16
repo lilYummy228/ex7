0be193eccc8695c70badb9179aa7cf672e228cdd
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-08-16 11:43:37 +0500

    Добавьте файлы проекта.

---
+++ ex7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите текст: ");
+            string text = Console.ReadLine();
+            Console.Write("Введите кол-во повторений: ");
+            int replay = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < replay; i++)
+            {
+                Console.WriteLine(text);
+            }
+        }
+    }
+}
